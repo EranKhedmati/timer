0cca693460de1c24f83df7fcb9d9c049db9c11a5
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-03-16 17:00:49 +0330

    combile the increase and decrease function(decrease check function added)

--- public/script/timer.js
+++ public/script/timer.js
@@ -1,6 +1,7 @@
 //Elements
 const timer = document.getElementById("timer"),
-  increase = document.querySelectorAll(".increse");
+  increase = document.querySelectorAll(".increse"),
+  decrease = document.querySelectorAll(".decrease");
 
 // Base Timer variable
 let sec = 0,
@@ -24,58 +25,101 @@ function handleUi(num) {
 }
 
 // check timer
-function checkTimer () {
-    if(sec > 59) {
-        min += 1;
-        sec -= 60;
-        if(min >59) {
-            min -= 60;
-            hour+= 1;
-        }
+function checkTimer() {
+  // increase
+  if (sec > 59) {
+    min += 1;
+    sec -= 60;
+    if (min > 59) {
+      min -= 60;
+      hour += 1;
     }
-    if(min >59) {
-        hour += 1;
-        min -=60;
+  }
+  if (min > 59) {
+    hour += 1;
+    min -= 60;
+  }
+
+  // decrease
+  if (sec < 0) {
+    min -= 1;
+    sec = 60 + sec;
+    if (min < 0 && hour == 0) {
+      hour = 0;
+      min = 0;
+      sec = 0;
+    } else if (min < 0 && hour > 0) {
+      min = 60 + min;
+      hour -= 1;
     }
-}   
+  }
+  if (min < 0 && hour == 0) {
+    hour = 0;
+    min = 0;
+    sec = 0;
+  } else if (min < 0 && hour > 0) {
+    min = 60 + min;
+    hour -= 1;
+  }
+}
+
+// change timer function
+function changeTimer(type, sign, num) {
+  switch (type) {
+    case "sec":
+      sign == "plus" ? (sec += num) : (sec -= num);
+      checkTimer();
+      updateTimer();
+      break;
+    case "min":
+      sign == "plus" ? (min += num) : (min -= num);
+      checkTimer();
+      updateTimer();
+      break;
+    case "hour": //its not working for now
+      sign == "plus" ? (hour += num) : (hour -= num);
+      checkTimer();
+      updateTimer();
+      break;
+  }
+}
 
 // Increse --------------------------------------------------------
 increase.forEach((btn) => {
   btn.addEventListener("click", () => {
     switch (btn.id) {
       case "30":
-        increment("sec", 30);
+        changeTimer("sec", "plus", 30);
         break;
       case "1":
-        increment("min", 1);     
+        changeTimer("min", "plus", 1);
         break;
       case "5":
-        increment("min", 5);
+        changeTimer("min", "plus", 5);
         break;
       case "10":
-        increment("min", 10);
+        changeTimer("min", "plus", 10);
         break;
     }
   });
 });
 
-// increse function
-function increment(type, num) {
-  switch (type) {
-    case "sec":
-      sec += num;
-      checkTimer()
-      updateTimer();
-      break;
-    case "min":
-      min += num;
-      checkTimer()
-      updateTimer();
-      break;
-    case "hour": //its not working for now
-      hour += num;
-      checkTimer()
-      updateTimer();
-      break;
-  }
-}
+// Decrease ------------------------------------------------------
+decrease.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    switch (btn.id) {
+      case "-30":
+        changeTimer("sec", "minus", 30);
+        break;
+      case "-1":
+        changeTimer("min", "minus", 1);
+        break;
+      case "-5":
+        changeTimer("min", "minus", 5);
+        break;
+      case "-10":
+        changeTimer("min", "minus", 10);
+        break;
+    }
+  });
+});
